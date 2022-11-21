d6e1be21e4032469b9046e8dbd6bf34161b0b70b
juliflorezg <forezjuli08@hotmail.com>
2022-11-21 13:18:12 -0500

    feat: add content to ProductGroup component

--- react/components/ProductGroup.tsx
+++ react/components/ProductGroup.tsx
@@ -1,9 +1,47 @@
 import React from "react";
 
-const ProductGroup = () =>{
+// interface Props {
+//   products: items[]
+// }
+
+// type items = {
+//   additionalInfo: {
+//     brandName: string
+//     __typeName: string
+//   }
+//   id: string
+//   price: number
+//   sellingPrice: number
+// }
+
+type Props = {
+  products: any[]
+}
+
+// const ProductGroup:React.FC<Props> = (products) =>{
+const ProductGroup = ({products}: Props) =>{
+  console.log("Mis productos en ProductGroup::::", products)
+
   return (
-    <div>Mi listado de productos</div>
+    <div>Mi listado de productos
+
+    {products.map((product: any, index: number) => (
+      <div key={index}>
+        <div>
+          <img src={product.imageUrls.at1x} alt={`imagen para producto ${product.name}`} />
+        </div>
+        <p>{product.name}</p>
+        <p>{product.id}</p>
+        <p>${product.price / 100}</p>
+        <p>Cant:{product.quantity}</p>
+      </div>
+    ))}
+
+    </div>
+
   )
+
+
 }
 
 
