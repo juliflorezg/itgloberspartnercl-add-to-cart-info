0c3579888e5bbbe1f1053024629985643b069e1d
juliflorezg <forezjuli08@hotmail.com>
2022-11-21 16:59:08 -0500

    feat: add CSS Handles to AddToCartInfo component

--- react/components/AddToCartInfo.tsx
+++ react/components/AddToCartInfo.tsx
@@ -4,8 +4,11 @@ import { useOrderForm } from "vtex.order-manager/OrderForm"
 import ButtonGroup from "./ButtonGroup"
 import Totalizers from "./Totalizers"
 import ProductGroup from "./ProductGroup"
+import { generateBlockClass } from "@vtex/css-handles"
+import styles from './styles.css'
 
-const AddToCartInfo = () => {
+const AddToCartInfo = ({blockClass}: {blockClass: string}) => {
+  const classes = generateBlockClass(styles.container, blockClass)
   const productInfo = useProduct()
   const {
     orderForm: {
@@ -18,19 +21,19 @@ const AddToCartInfo = () => {
 
   console.log("Información de mi producto:", productInfo);
   console.log("~~~~~~~~~~~Mi info de orden:::", items);
-  console.log("~~~~~~~~~~~Totales:::", totalizers); // [], there are no totalizers for this orden
+  console.log("~~~~~~~Totales:::", totalizers); // [], there are no totalizers for this orden
 
 
   return (
     // <div>Aqui se renderizará mi info jejeje</div>
-    <>
+    <div className={classes}>
       {/* listado de productos */}
       <ProductGroup products={items}/>
       {/* valor total */}
       <Totalizers totalizers={totalizers[0] ?? {}} quantity={totalItems}/>
       {/* acciones */}
       <ButtonGroup />
-    </>
+    </div>
   )
 }
 
