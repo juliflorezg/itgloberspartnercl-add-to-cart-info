4bd135828f76ebff2fff06b336c33c0551b5972d
juliflorezg <forezjuli08@hotmail.com>
2022-11-21 10:35:04 -0500

    feat: add ButtonGroup component

---
+++ react/components/ButtonGroup.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ButtonGroup = () => {
+  return (
+    <>
+      <div>
+        <a href="/">CHECK OUT</a>
+      </div>
+      <div>
+        <button>CONTINÚA COMPRANDO</button>
+        <a href="/">VER TU BOLSA</a>
+      </div>
+    </>
+  )
+}
+
+export default ButtonGroup
