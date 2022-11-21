70ea0a9edc71e1d78d9e517cffa84c0cf761ecda
juliflorezg <forezjuli08@hotmail.com>
2022-11-21 10:35:55 -0500

    feat: add Totalizers component

---
+++ react/components/Totalizers.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Totalizers = () => {
+  return (
+    <div>
+      <p>Tenemos x items en tu compra</p>
+      <p>Total: xxx</p>
+    </div>
+  )
+}
+
+export default Totalizers
