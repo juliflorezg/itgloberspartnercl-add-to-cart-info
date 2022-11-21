cfba68f6fbe5ab5e9dd57f67bef25ec5800a3f5d
juliflorezg <forezjuli08@hotmail.com>
2022-11-21 13:18:36 -0500

    feat: add content to Totalizers component

--- react/components/Totalizers.tsx
+++ react/components/Totalizers.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 
-const Totalizers = () => {
+interface Props {
+  totalizers : {
+    id: string
+    name: string
+    value: number
+  }
+  quantity: number
+}
+
+const Totalizers = ({totalizers: {id = "123", name = "lol", value = 6669}, quantity}: Props) => {
+  console.log(id, name, value);
   return (
     <div>
-      <p>Tenemos x items en tu compra</p>
-      <p>Total: xxx</p>
+      <p>Tenemos {quantity} items en tu compra</p>
+      <p>Total: {value/100}</p>
     </div>
   )
 }
