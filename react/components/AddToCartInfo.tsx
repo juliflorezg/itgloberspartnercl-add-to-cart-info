43d29e87414f7128c5c3e03e1a728a741aeaf4de
juliflorezg <forezjuli08@hotmail.com>
2022-11-21 13:19:08 -0500

    feat: add content to AddToCartInfo component

--- react/components/AddToCartInfo.tsx
+++ react/components/AddToCartInfo.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import { useProduct } from "vtex.product-context"
 import { useOrderForm } from "vtex.order-manager/OrderForm"
 import ButtonGroup from "./ButtonGroup"
-import Totalizers from "./Totalizer"
+import Totalizers from "./Totalizers"
 import ProductGroup from "./ProductGroup"
 
 const AddToCartInfo = () => {
@@ -12,17 +12,22 @@ const AddToCartInfo = () => {
       items,
       totalizers
   } } = useOrderForm()
+
+  const totalItems = items.map((item: any) => item.quantity).reduce((curr:number , acc: number) => curr + acc, 0)
+
+
   console.log("Información de mi producto:", productInfo);
-  console.log("~~~~~~~~~~~Mi info de orden:::", items, totalizers);
+  console.log("~~~~~~~~~~~Mi info de orden:::", items);
+  console.log("~~~~~~~~~~~Totales:::", totalizers); // [], there are no totalizers for this orden
 
 
   return (
     // <div>Aqui se renderizará mi info jejeje</div>
     <>
       {/* listado de productos */}
-      <ProductGroup />
+      <ProductGroup products={items}/>
       {/* valor total */}
-      <Totalizers />
+      <Totalizers totalizers={totalizers[0] ?? {}} quantity={totalItems}/>
       {/* acciones */}
       <ButtonGroup />
     </>
